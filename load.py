05f4876d5cf20a7b14c16a28d1aac29ec13d488a
Oluwasayo <oluwasayo.dev@gmail.com>
2023-03-18 14:27:57 +0100

    include datetime column to data to be loaded

--- load.py
+++ load.py
@@ -1,13 +1,15 @@
-import psycopg2
 import pandas as pd
 
 
 
 def load_csv_to_postgres(path:str, db_connection):
     df = pd.read_csv(path)
+    df.columns = [x.lower() for x in df.columns]
+    df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], format="%d/%m/%Y %H:%M")
+    df.drop(columns=['date', 'time'], inplace=True)
     df.to_sql(
-        name=path.rstrip('/').split('.')[0],
-        if_exists='replace',
+        name='goalbet',
+        if_exists='append',
         index=False,
         con=db_connection
     )
