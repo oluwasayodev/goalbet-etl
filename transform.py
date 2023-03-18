685e3930f051498f083f753a3bd878754e7bc743
Oluwasayo <oluwasayo.dev@gmail.com>
2023-03-18 14:28:53 +0100

    added format_date_column and add_time_column transformations

--- transform.py
+++ transform.py
@@ -11,3 +11,23 @@ def extract_columns(path: str, columns: list[str]):
         cp.mkdir()
     df.to_csv(f'{cp.name}/{fp.name}', index=False)
     return f'{cp.name}/{fp.name}'
+
+def format_date_column(path: str, date_column_name='date', format='%d/%m/%Y'):
+    fp = Path(path)
+    df = pd.read_csv(path, parse_dates=[date_column_name], on_bad_lines='warn')
+    df[date_column_name] = df[date_column_name].dt.strftime('%d/%m/%Y')
+    cp = Path('transformed')
+    if not cp.exists():
+        cp.mkdir()
+    df.to_csv(f'{cp.name}/{fp.name}', index=False)
+    return f'{cp.name}/{fp.name}'
+
+def add_time_column(path):
+    fp = Path(path)
+    df = pd.read_csv(path, on_bad_lines='warn')
+    df["Time"] = "00:00"
+    cp = Path('transformed')
+    if not cp.exists():
+        cp.mkdir()
+    df.to_csv(f'{cp.name}/{fp.name}', index=False)
+    return f'{cp.name}/{fp.name}'
